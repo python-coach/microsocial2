8f560ed7a7ec169be8b58f2e21fd11856d9df885
liminspace <igormeln@gmail.com>
2015-06-30 17:34:01 +0300

    Add static dir.

--- microsocial2/settings.py
+++ microsocial2/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
